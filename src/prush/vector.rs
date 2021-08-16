f5710bfacbe29609087f5b2c71908387f92a417b
johker <johannes.kern@zoho.com>
2021-08-17 00:14:37 +0200

    Added basic arithmetic instructions for float vectoor

--- src/prush/vector.rs
+++ src/prush/vector.rs
@@ -438,6 +438,102 @@ pub fn int_vector_yank_dup(push_state: &mut PushState, _instruction_cache: &Inst
 
 ////////////////////////////////////// FLOATVECTOR //////////////////////////////////////////
 
+/// FLOATVECTOR.ADD: Pushes the result of applying element-wise ADD of the top item to the
+/// second item on the FLOATVECTOR stack. It applies an offset to the indices of the top
+/// item. The offset is taken from the INTEGER stack. Indices that are outside of the valid
+/// range of the second item are ignored. If there is no overlap of indices the second item of
+/// the stack is pushed as a result.
+pub fn float_vector_add(push_state: &mut PushState, _instruction_cache: &InstructionCache) {
+    if let Some(mut iv) = push_state.float_vector_stack.pop_vec(2) {
+        if let Some(offset) = push_state.int_stack.pop() {
+            // Loop through indices of second item
+            let scd_size = iv[0].values.len();
+            for i in 0..scd_size {
+                let ofs_idx = (i as i32 + offset) as usize;
+                if ofs_idx > scd_size - 1 {
+                    continue; // Out of bounds
+                }
+                iv[0].values[ofs_idx] += iv[1].values[i];
+            }
+            push_state.float_vector_stack.push(iv[0].clone());
+        }
+    }
+}
+
+/// FLOATVECTOR.SUBTRACT: Pushes the result of element-wise SUBTRACT of the top item from the
+/// second item on the INTVECTOR stack. It applies an offset to the indices of the top
+/// item. The offset is taken from the INTEGER stack. Indices that are outside of the valid
+/// range of the second item are ignored. If there is no overlap of indices the second item of
+/// the stack is pushed as a result.
+pub fn float_vector_subtract(push_state: &mut PushState, _instruction_cache: &InstructionCache) {
+    if let Some(mut iv) = push_state.float_vector_stack.pop_vec(2) {
+        if let Some(offset) = push_state.int_stack.pop() {
+            // Loop through indices of second item
+            let scd_size = iv[0].values.len();
+            for i in 0..scd_size {
+                let ofs_idx = (i as i32 + offset) as usize;
+                if ofs_idx > scd_size - 1 {
+                    continue; // Out of bounds
+                }
+                iv[0].values[ofs_idx] -= iv[1].values[i];
+            }
+            push_state.float_vector_stack.push(iv[0].clone());
+        }
+    }
+}
+
+/// INTVECTOR.MULTIPLY: Pushes the result of element-wise MULTIPLY of the top item to the
+/// second item on the INTVECTOR stack. It applies an offset to the indices of the top
+/// item. The offset is taken from the INTEGER stack. Indices that are outside of the valid
+/// range of the second item are ignored. If there is no overlap of indices the second item of
+/// the stack is pushed as a result.
+pub fn float_vector_multiply(push_state: &mut PushState, _instruction_cache: &InstructionCache) {
+    if let Some(mut iv) = push_state.float_vector_stack.pop_vec(2) {
+        if let Some(offset) = push_state.int_stack.pop() {
+            // Loop through indices of second item
+            let scd_size = iv[0].values.len();
+            for i in 0..scd_size {
+                let ofs_idx = (i as i32 + offset) as usize;
+                if ofs_idx > scd_size - 1 {
+                    continue; // Out of bounds
+                }
+                iv[0].values[ofs_idx] *= iv[1].values[i];
+            }
+            push_state.float_vector_stack.push(iv[0].clone());
+        }
+    }
+}
+
+/// FLOATVECTOR.DIVIDE: Pushes the result of element-wise DIVIDE of the second item by the
+/// top item on the INTVECTOR stack. It applies an offset to the indices of the top
+/// item. The offset is taken from the INTEGER stack. Indices that are outside of the valid
+/// range of the second item are ignored. If there is no overlap of indices the second item of
+/// the stack is pushed as a result. If at least one divisor is zero the instruction acts
+/// as NOOP.
+pub fn float_vector_divide(push_state: &mut PushState, _instruction_cache: &InstructionCache) {
+    if let Some(mut iv) = push_state.float_vector_stack.pop_vec(2) {
+        if let Some(offset) = push_state.int_stack.pop() {
+            let mut invalid = false;
+            // Loop through indices of second item
+            let scd_size = iv[0].values.len();
+            for i in 0..scd_size {
+                let ofs_idx = (i as i32 + offset) as usize;
+                if ofs_idx > scd_size - 1 {
+                    continue; // Out of bounds
+                }
+                if iv[1].values[i] == 0.0 {
+                    invalid = true;
+                } else {
+                    iv[0].values[ofs_idx] /= iv[1].values[i];
+                }
+            }
+            if !invalid {
+                push_state.float_vector_stack.push(iv[0].clone());
+            }
+        }
+    }
+}
+
 /// FLOATVECTOR.DEFINE: Defines the name on top of the NAME stack as an instruction that will
 /// push the top item of the FLOATVECTOR stack onto the EXEC stack.
 pub fn float_vector_define(push_state: &mut PushState, _instruction_cache: &InstructionCache) {
@@ -1024,6 +1120,77 @@ mod tests {
     fn float_vector_prints_values() {
         let fv = FloatVector::new(vec![1.2, 3.4, -4.5]);
         assert_eq!(fv.to_string(), "[1.2,3.4,-4.5]");
+    }
+    #[test]
+    fn float_vector_add_with_partial() {
+        let test_vec1 = FloatVector::new(vec![1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0]);
+        let test_vec2 = FloatVector::new(vec![1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0]);
+
+        // Full overlap
+        let mut test_state = PushState::new();
+        test_state.float_vector_stack.push(test_vec2.clone());
+        test_state.float_vector_stack.push(test_vec1.clone());
+        test_state.int_stack.push(0);
+        float_vector_add(&mut test_state, &icache());
+        assert_eq!(test_state.float_vector_stack.size(), 1);
+        assert_eq!(
+            test_state.float_vector_stack.pop().unwrap(),
+            FloatVector::new(vec![2.0, 1.0, 2.0, 1.0, 1.0, 0.0, 1.0, 0.0])
+        );
+    }
+
+    #[test]
+    fn float_vector_subtract_with_partial_overlap() {
+        let test_vec1 = FloatVector::new(vec![1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0]);
+        let test_vec2 = FloatVector::new(vec![1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0]);
+
+        // Full overlap
+        let mut test_state = PushState::new();
+        test_state.float_vector_stack.push(test_vec2.clone());
+        test_state.float_vector_stack.push(test_vec1.clone());
+        test_state.int_stack.push(4);
+        float_vector_subtract(&mut test_state, &icache());
+        assert_eq!(test_state.float_vector_stack.size(), 1);
+        assert_eq!(
+            test_state.float_vector_stack.pop().unwrap(),
+            FloatVector::new(vec![1.0, 0.0, 1.0, 0.0, 0.0, -1.0, 0.0, -1.0])
+        );
+    }
+
+    #[test]
+    fn float_vector_multiply_with_partial_overlap() {
+        let test_vec1 = FloatVector::new(vec![2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0]);
+        let test_vec2 = FloatVector::new(vec![1.0, 3.0, 1.0, 3.0, 1.0, 3.0, 1.0, 3.0]);
+
+        // Full overlap
+        let mut test_state = PushState::new();
+        test_state.float_vector_stack.push(test_vec2.clone());
+        test_state.float_vector_stack.push(test_vec1.clone());
+        test_state.int_stack.push(4);
+        float_vector_multiply(&mut test_state, &icache());
+        assert_eq!(test_state.float_vector_stack.size(), 1);
+        assert_eq!(
+            test_state.float_vector_stack.pop().unwrap(),
+            FloatVector::new(vec![1.0, 3.0, 1.0, 3.0, 2.0, 6.0, 2.0, 6.0])
+        );
+    }
+
+    #[test]
+    fn float_vector_divide_with_partial_overlap() {
+        let test_vec1 = FloatVector::new(vec![2.0, 2.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0]);
+        let test_vec2 = FloatVector::new(vec![6.0, 4.0, 6.0, 4.0, 6.0, 4.0, 6.0, 4.0]);
+
+        // Full overlap
+        let mut test_state = PushState::new();
+        test_state.float_vector_stack.push(test_vec2.clone());
+        test_state.float_vector_stack.push(test_vec1.clone());
+        test_state.int_stack.push(4);
+        float_vector_divide(&mut test_state, &icache());
+        assert_eq!(test_state.float_vector_stack.size(), 1);
+        assert_eq!(
+            test_state.float_vector_stack.pop().unwrap(),
+            FloatVector::new(vec![6.0, 4.0, 6.0, 4.0, 3.0, 2.0, 3.0, 2.0])
+        );
     }
 
     #[test]
