7bb8733a12cd9b4aed311a42b79de79378e31e65
johker <johannes.kern@zoho.com>
2021-10-08 16:26:16 +0200

    Fixed testing / added flag for default imputation sign

--- src/push/sorting.rs
+++ src/push/sorting.rs
@@ -33,30 +33,33 @@ impl Sorting {
         return None;
     }
 
-    pub fn heap_sort(arr: &mut [Item]) {
+    /// Sorts the array in descending order which results
+    /// in a ascending order for the stack. The items with lowest
+    /// values are placed at the top of the stack.
+    pub fn heap_sort(arr: &mut [Item], pos_default: &bool) {
         if arr.len() <= 1 {
             return; // already sorted
         }
 
-        Sorting::heapify(arr);
+        Sorting::heapify(arr, pos_default);
 
         for end in (1..arr.len()).rev() {
             arr.swap(0, end);
-            Sorting::move_down(&mut arr[..end], 0);
+            Sorting::move_down(&mut arr[..end], 0, pos_default);
         }
     }
 
     /// Convert 'arr' into a max heap.
-    fn heapify(arr: &mut [Item]) {
+    fn heapify(arr: &mut [Item], pos_default: &bool) {
         let last_parent = (arr.len() - 2) / 2;
         for i in (0..=last_parent).rev() {
-            Sorting::move_down(arr, i);
+            Sorting::move_down(arr, i, pos_default);
         }
     }
 
-    /// Move the element at 'root' down until 'arr' is a max heap again.
-    /// This assumes that the subtrees under `root` are valid max heaps already.
-    fn move_down(arr: &mut [Item], mut root: usize) {
+    /// Move the element at 'root' down until 'arr' is a min heap again.
+    /// This assumes that the subtrees under `root` are valid min heaps already.
+    fn move_down(arr: &mut [Item], mut root: usize, pos_default: &bool) {
         let last = arr.len() - 1;
         loop {
             let left = 2 * root + 1;
@@ -64,61 +67,33 @@ impl Sorting {
                 break;
             }
             let right = left + 1;
-            let max = if right <= last && Sorting::uvalue(&arr[right]) > Sorting::uvalue(&arr[left])
+            let max = if right <= last
+                && Sorting::value(&arr[right], pos_default)
+                    > Sorting::value(&arr[left], pos_default)
             {
                 right
             } else {
                 left
             };
 
-            if Sorting::uvalue(&arr[max]) > Sorting::uvalue(&arr[root]) {
+            if Sorting::value(&arr[max], pos_default) > Sorting::value(&arr[root], pos_default) {
                 arr.swap(root, max);
             }
             root = max;
         }
     }
 
-    /// Heap sort with descending order
-    //   pub fn desc_heap_sort(push_state: &mut PushState, n: usize) {
-    //        // Build heap (rearrange array)
-    //        for (int i = n / 2 - 1; i >= 0; i--)
-    //            min_heapify(push_state, n, i);
-    //   }
-
-    /// Heapifies a subtree rooted with node i which is an index in the list.
-    /// It uses a min heap and obtains stack values by list_uvalue for sorting
-    /// in descending order.
-    //    pub fn min_heapify(push_state: &mut PushState, n: usize, i: usize) {
-    //        let mut smallest = i; // Initialize smallest as root
-    //        let l = 2 * i + 1;
-    //        let r = 2 * i + 2;
-    //
-    //        // If left child is smaller than root
-    //        if l < n && Sorting::list_lvalue(push_state, l) < Sorting::list_lvalue(push_state, smallest)
-    //        {
-    //            smallest = l;
-    //        }
-    //
-    //        // If right child is smaller than smallest so far
-    //        if r < n && Sorting::list_lvalue(push_state, r) < Sorting::list_lvalue(push_state, smallest)
-    //        {
-    //            smallest = r;
-    //        }
-    //
-    //        // If smallest is not root
-    //        if smallest != i {
-    //            push_state.code_stack.swap(i, smallest);
-    //
-    //            // Recursively heapify the affected sub-tree
-    //            Sorting::min_heapify(push_state, n,  smallest);
-    //        }
-    //    }
-
     /// Extracts the sort value from the list item.
     /// The sort value is defined as the item below the id (stack position 2).
     /// The function returns f32::INFINITY if no list with at least two items is found or
     /// if the second item does not have type INT/FLOAT.
-    pub fn uvalue(item: &Item) -> f32 {
+    pub fn value(item: &Item, pos_default: &bool) -> f32 {
+        let default_value;
+        if *pos_default {
+            default_value = f32::INFINITY;
+        } else {
+            default_value = f32::NEG_INFINITY;
+        }
         match item {
             Item::List { items } => {
                 if items.size() >= 2 {
@@ -126,48 +101,21 @@ impl Sorting {
                         Some(Item::Literal { push_type }) => match push_type {
                             PushType::Int { val } => return val.clone() as f32,
                             PushType::Float { val } => return val.clone(),
-                            _ => return f32::INFINITY,
+                            _ => return default_value,
                         },
-                        _ => return f32::INFINITY,
+                        _ => return default_value,
                     }
                 }
             }
-            _ => return f32::INFINITY,
+            _ => return default_value,
         }
-        return f32::INFINITY;
-    }
-
-    /// Extracts the sort value from the list item.
-    /// The sort value is defined as the item below the id (stack position 2).
-    /// The function returns f32::NEG_INFINITY if no list with at least two items is found or
-    /// if the second item does not have type INT/FLOAT.
-    pub fn lvalue(item: &Item) -> f32 {
-        match item {
-            Item::List { items } => {
-                if items.size() >= 2 {
-                    match items.get(1) {
-                        Some(Item::Literal { push_type }) => match push_type {
-                            PushType::Int { val } => return val.clone() as f32,
-                            PushType::Float { val } => return val.clone(),
-                            _ => return f32::NEG_INFINITY,
-                        },
-                        _ => return f32::NEG_INFINITY,
-                    }
-                }
-            }
-            _ => return f32::NEG_INFINITY,
-        }
-        return f32::NEG_INFINITY;
+        return default_value;
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    pub fn icache() -> InstructionCache {
-        InstructionCache::new(vec![])
-    }
 
     /// Creates a test list entry with the given
     /// value to sort and the id = 0.
@@ -192,39 +140,46 @@ mod tests {
             Item::list(vec![Item::float(1.0), Item::int(4)]),
             Item::list(vec![Item::int(9), Item::int(3)]),
         ];
-        assert_eq!(Sorting::uvalue(&test_array[0]), 2.0);
-        assert_eq!(Sorting::uvalue(&test_array[1]), 1.0);
-        assert_eq!(Sorting::uvalue(&test_array[2]), 9.0);
-        assert_eq!(Sorting::lvalue(&test_array[0]), 2.0);
-        assert_eq!(Sorting::lvalue(&test_array[1]), 1.0);
-        assert_eq!(Sorting::lvalue(&test_array[2]), 9.0);
+        let pos_default = true;
+        assert_eq!(Sorting::value(&test_array[0], &pos_default), 2.0);
+        assert_eq!(Sorting::value(&test_array[1], &pos_default), 1.0);
+        assert_eq!(Sorting::value(&test_array[2], &pos_default), 9.0);
+        let pos_default = false;
+        assert_eq!(Sorting::value(&test_array[0], &pos_default), 2.0);
+        assert_eq!(Sorting::value(&test_array[1], &pos_default), 1.0);
+        assert_eq!(Sorting::value(&test_array[2], &pos_default), 9.0);
     }
 
     #[test]
     fn extract_sort_value_returns_infinity_when_item_not_found() {
         let test_item = Item::list(vec![Item::int(2)]);
-        assert_eq!(Sorting::uvalue(&test_item), f32::INFINITY);
-        assert_eq!(Sorting::lvalue(&test_item), f32::NEG_INFINITY);
+        let pos_default = true;
+        assert_eq!(Sorting::value(&test_item, &pos_default), f32::INFINITY);
+        let pos_default = false;
+        assert_eq!(Sorting::value(&test_item, &pos_default), f32::NEG_INFINITY);
     }
 
     #[test]
     fn empty() {
         let mut arr: Vec<Item> = Vec::new();
-        Sorting::heap_sort(&mut arr);
+        let pos_default = true;
+        Sorting::heap_sort(&mut arr, &pos_default);
         assert_eq!(&arr, &[]);
     }
 
     #[test]
     fn single_element() {
         let mut arr = vec![litem(1)];
-        Sorting::heap_sort(&mut arr);
+        let pos_default = true;
+        Sorting::heap_sort(&mut arr, &pos_default);
         assert!(Item::equals(&arr[0], &litem(1)));
     }
 
     #[test]
     fn sorted_array() {
         let mut arr = vec![litem(1), litem(2), litem(3), litem(4)];
-        Sorting::heap_sort(&mut arr);
+        let pos_default = true;
+        Sorting::heap_sort(&mut arr, &pos_default);
         assert!(Item::equals(&arr[0], &litem(1)));
         assert!(Item::equals(&arr[1], &litem(2)));
         assert!(Item::equals(&arr[2], &litem(3)));
@@ -234,7 +189,8 @@ mod tests {
     #[test]
     fn unsorted_array() {
         let mut arr = vec![litem(3), litem(4), litem(2), litem(1)];
-        Sorting::heap_sort(&mut arr);
+        let pos_default = true;
+        Sorting::heap_sort(&mut arr, &pos_default);
         assert!(Item::equals(&arr[0], &litem(1)));
         assert!(Item::equals(&arr[1], &litem(2)));
         assert!(Item::equals(&arr[2], &litem(3)));
@@ -242,9 +198,27 @@ mod tests {
     }
 
     #[test]
+    fn unsorted_array_with_empty_items() {
+        let mut arr = vec![litem(3), Item::int(4), litem(2), litem(1)];
+        let pos_default = true;
+        Sorting::heap_sort(&mut arr, &pos_default);
+        assert!(Item::equals(&arr[0], &litem(1)));
+        assert!(Item::equals(&arr[1], &litem(2)));
+        assert!(Item::equals(&arr[2], &litem(3)));
+        assert!(Item::equals(&arr[3], &Item::int(4)));
+        let pos_default = false;
+        Sorting::heap_sort(&mut arr, &pos_default);
+        assert!(Item::equals(&arr[0], &Item::int(4)));
+        assert!(Item::equals(&arr[1], &litem(1)));
+        assert!(Item::equals(&arr[2], &litem(2)));
+        assert!(Item::equals(&arr[3], &litem(3)));
+    }
+
+    #[test]
     fn odd_number_of_elements() {
         let mut arr = vec![litem(3), litem(4), litem(2), litem(1), litem(7)];
-        Sorting::heap_sort(&mut arr);
+        let pos_default = true;
+        Sorting::heap_sort(&mut arr, &pos_default);
         assert!(Item::equals(&arr[0], &litem(1)));
         assert!(Item::equals(&arr[1], &litem(2)));
         assert!(Item::equals(&arr[2], &litem(3)));
@@ -255,7 +229,8 @@ mod tests {
     #[test]
     fn repeated_elements() {
         let mut arr = vec![litem(542), litem(542), litem(542), litem(542)];
-        Sorting::heap_sort(&mut arr);
+        let pos_default = true;
+        Sorting::heap_sort(&mut arr, &pos_default);
         assert!(Item::equals(&arr[0], &litem(542)));
         assert!(Item::equals(&arr[0], &litem(542)));
         assert!(Item::equals(&arr[0], &litem(542)));
