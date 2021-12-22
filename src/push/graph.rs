29e8dea1f8e51d10869ab5eba9fd037639e7e393
johker <johannes.kern@zoho.com>
2021-12-22 23:11:41 +0100

    Added functions to remove nodes and edges

--- src/push/graph.rs
+++ src/push/graph.rs
@@ -28,7 +28,7 @@ impl PartialEq for Node {
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Copy, Clone, Debug)]
 pub struct Edge {
     node_id: usize,
     weight: f32,
@@ -65,6 +65,8 @@ pub struct Graph {
 }
 
 impl Graph {
+    /// Adds an new node with the given state and activity
+    /// and returns its assigned IDs.
     pub fn add_node(&mut self, state: i32, active: bool) -> usize {
         let node = Node::new(state, active);
         let node_id = node.node_id;
@@ -73,6 +75,19 @@ impl Graph {
         node_id.clone()
     }
 
+    /// Removesthe node with the given id and
+    /// all its outgoing connections from the graph.
+    pub fn remove_node(&mut self, id: usize) {
+        self.nodes.remove(&id);
+        self.edges.remove(&id);
+        for (_, edges) in self.edges.iter_mut() {
+            edges.remove(&Edge::new(id, 0.0));
+        }
+    }
+
+    /// Adds a connection to the directed graph from the
+    /// node with origin_id to destination_id and assigns the
+    /// weight parameter to it.
     pub fn add_connection(&mut self, origin_id: usize, destination_id: usize, weight: f32) {
         if self.nodes.contains_key(&origin_id) && self.nodes.contains_key(&destination_id) {
             if let Some(incoming_edges) = self.edges.get_mut(&destination_id) {
@@ -80,6 +95,14 @@ impl Graph {
                 let edge = Edge::new(origin_id, weight);
                 incoming_edges.insert(edge);
             }
+        }
+    }
+
+    /// Removes the connection from origin_id to destination_id
+    /// from the graph
+    pub fn remove_connection(&mut self, origin_id: usize, destination_id: usize) {
+        if let Some(incoming_edges) = self.edges.get_mut(&destination_id) {
+            incoming_edges.remove(&Edge::new(origin_id, 0.0));
         }
     }
 }
